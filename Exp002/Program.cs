092755b7b56c7f5db86708123d8c1914dbf2f465
Rostislav <littamb69@gmail.com>
2022-09-30 20:04:18 +0600

    Добавил приветствие

--- Exp002/Program.cs
+++ Exp002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Rostislav!");
+﻿Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет");
+Console.Write(username);
